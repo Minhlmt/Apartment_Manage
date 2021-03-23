1a2e68ef98c36c6fef216d80bd8873d8b3bd0867
minh <hoangminhlmt@gmail.com>
2021-03-23 22:01:26 +0700

    push notify

--- App.js
+++ App.js
@@ -7,9 +7,54 @@ import {Tab_Home_Profile} from './src/globals/screen'
 import Apartment from './src/components/Apartment/Apartment'
 import { ScreenKey } from './src/globals/constants';
 import SignIn from './src/components/SignIn/SignIn'
+// import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
+import Firesase from '@react-native-firebase/app' 
 const MainNavigationStack = createStackNavigator();
 export default function App({navigation}) {
   const [checkToken, setCheckToken] = useState(false);
+  useEffect(()=>{
+    Firesase.initializeApp();
+    PushNotification.configure({
+   
+      onRegister: function (token) {
+        console.log("TOKEN:", token);
+      },
+    
+    
+      onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+    
+      
+        // notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+    
+      onAction: function (notification) {
+        console.log("ACTION:", notification.action);
+        console.log("NOTIFICATION:", notification);
+    
+      
+      },
+    
+     
+      onRegistrationError: function(err) {
+        console.error(err.message, err);
+      },
+    
+     
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+    
+    
+      popInitialNotification: true,
+    
+    
+      requestPermissions: true,
+    });
+  })
   
   
   // useEffect(()=>{
---
+++ src/components/Info/ChangeInfo.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, SectionList, Text, View, Image, TouchableOpacity, Button,TextInput } from 'react-native';
+export default function ChangeInfo(){
+    return(
+        <View>
+            <Text></Text>
+            <TextInput></TextInput>
+        </View>
+    )
+}
